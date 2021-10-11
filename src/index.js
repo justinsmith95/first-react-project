92f3b8a29204d926d28bb1d2f96dd016753f2bd6
jtsmith9562 <jtsmith9562@gmail.com>
2021-10-11 16:31:08 -0400

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
